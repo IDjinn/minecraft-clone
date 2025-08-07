13863cbc21be6bec3a697a30cbf0728131a39b18
IDjinn <42049518+IDjinn@users.noreply.github.com>
2025-08-07 00:23:15 -0300

    render world progress

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,32 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #define WIDTH 1920
 #define HEIGHT 1024
+#define CAMERA_VELOCITY 0.001
+#define SCROLL_VELOCITY 0.006
+#define SCROLL_DRAG 2.34325f
 
-void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int width, int height) {
+void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, const int width, const int height) {
     glViewport(0, 0, width, height);
 }
 
 const char *vertexShaderSource = R"glsl(
     #version 330 core
     layout(location = 0) in vec3 aPos;
+    layout(location = 1) in vec3 instancePos;
 
-    uniform mat4 model;
     uniform mat4 view;
     uniform mat4 projection;
 
     void main() {
-        gl_Position = projection * view * model * vec4(aPos, 1.0);
+        vec4 worldPosition = vec4(aPos + instancePos, 1.0);
+        gl_Position = projection * view * worldPosition;
     }
 )glsl";
 
@@ -39,6 +44,37 @@ const char *fragmentShaderSource = R"glsl(
     }
 )glsl";
 
+enum BlockType {
+    AIR,
+    GRASS
+};
+
+struct Block {
+    uint8_t x;
+    uint8_t y;
+    uint8_t z;
+
+    uint8_t blockType;
+
+    Block() = default;
+
+    Block(const int x, const int y, const int z, const BlockType blockType) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->blockType = blockType;
+    }
+};
+
+
+int32_t mouse_scroll_y_offset = 0;
+float mouse_scroll_drag = 0;
+
+void scroll_callback(GLFWwindow *window, [[maybe_unused]] double xoffset, const double yoffset) {
+    // xoffset is not used here (we will use mouse scroll)
+    mouse_scroll_y_offset = static_cast<int>(yoffset);
+    mouse_scroll_drag = SCROLL_DRAG;
+}
 
 int main() {
     if (!glfwInit()) {
@@ -66,9 +102,24 @@ int main() {
 
     glViewport(0, 0, WIDTH, HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    Block world[10][10][10] = {};
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            for (int z = 0; z < 10; z++) {
+                world[x][y][z] = Block(
+                    x,
+                    y,
+                    z,
+                    GRASS
+                );
+            }
+        }
+    }
 
 
-    float cubeVertices[] = {
+    constexpr float cubeVertices[] = {
         // positions
         // front face
         -0.5f, -0.5f, 0.5f,
@@ -145,54 +196,150 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void *>(nullptr));
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    const float faceVertices[6][18] = {
+        // front (0, 0, +1)
+        {
+            -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+            0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f
+        },
 
-    auto cameraPos = glm::vec3(1, 3, 3.0f);
+        // back (0, 0, -1)
+        {
+            -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
+            0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f
+        },
+
+        // left (-1, 0, 0)
+        {
+            -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f
+        },
+
+        // right (1, 0, 0)
+        {
+            0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f
+        },
+
+        // up (0, 1, 0)
+        {
+            -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+            0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f
+        },
+
+        // down (0, -1, 0)
+        {
+            -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f,
+            0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f
+        }
+    };
+
+    const int directions[6][3] = {
+        {0, 0, 1}, // front
+        {0, 0, -1}, // back
+        {-1, 0, 0}, // left
+        {1, 0, 0}, // right
+        {0, 1, 0}, // top
+        {0, -1, 0} // bottom
+    };
+
+    auto isSolid = [&](const int x, const int y, const int z) {
+        return x >= 0 && x < 10 && y >= 0 && y < 10 && z >= 0 && z < 10 && world[x][y][z].blockType != AIR;
+    };
+
+    std::vector<glm::vec3> visibleVertices{};
+    for (auto &x: world) {
+        for (auto &y: x) {
+            for (auto &z: y) {
+                auto block = &z;
+                if (block->blockType == AIR) continue;
+
+                for (int face = 0; face < 6; ++face) {
+                    auto neighboor_x = block->x + directions[face][0];
+                    auto neighboor_y = block->y + directions[face][1];
+                    auto neighboor_z = block->z + directions[face][2];
+                    if (isSolid(neighboor_x, neighboor_y, neighboor_z)) continue;
+
+                    for (int i = 0; i < 18; i += 3) {
+                        float vx = faceVertices[face][i] + block->x;
+                        float vy = faceVertices[face][i + 1] + block->y;
+                        float vz = faceVertices[face][i + 2] + block->z;
+                        visibleVertices.insert(visibleVertices.end(), {vx, vy, vz});
+                    }
+                }
+            }
+        }
+    }
+
+
+    unsigned int visibleVBO;
+    glGenBuffers(1, &visibleVBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, visibleVBO);
+    glBufferData(GL_ARRAY_BUFFER, visibleVertices.size() * sizeof(glm::vec3), visibleVertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+
+    auto cameraPos = glm::vec3(11, 6, 11);
+    auto draw_line = false;
     while (!glfwWindowShouldClose(window)) {
+        if (mouse_scroll_drag > 0) {
+            float offset = mouse_scroll_y_offset > 0 ? -(SCROLL_VELOCITY) : SCROLL_VELOCITY;
+            mouse_scroll_drag -= CAMERA_VELOCITY;
+            cameraPos += glm::vec3(offset, offset, 0);
+        }
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+            draw_line = !draw_line;
+        }
         if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-            cameraPos += glm::vec3(0, 0.001, 0);
+            cameraPos += glm::vec3(0, CAMERA_VELOCITY, 0);
         }
         if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-            cameraPos += glm::vec3(0, -0.001, 0);
+            cameraPos += glm::vec3(0, -(CAMERA_VELOCITY), 0);
         }
         if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-            cameraPos += glm::vec3(0.001, 0, 0);
+            cameraPos += glm::vec3(CAMERA_VELOCITY, 0, 0);
         }
         if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-            cameraPos += glm::vec3(-0.001, 0, 0);
+            cameraPos += glm::vec3(-(CAMERA_VELOCITY), 0, 0);
         }
 
+        glPolygonMode(GL_FRONT_AND_BACK, draw_line ? GL_LINE : GL_FILL);
+
         glClearColor(0, 0, 0, 0);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+        glEnable(GL_DEPTH_TEST);
 
         auto target = glm::vec3(0.0f, 0.0f, 0.0f);
         auto cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-        auto model = glm::mat4(1.0f);
         auto view = glm::lookAt(cameraPos, target, cameraUp);
-        auto projection = glm::perspective(glm::radians(45.0f), static_cast<float>(10) / 10, 0.1f, 100.0f);
+        auto projection = glm::perspective(glm::radians(45.0f), static_cast<float>(WIDTH) / HEIGHT, 0.1f, 100.0f);
 
-        auto modelLoc = glGetUniformLocation(shaderProgram, "model");
-        auto viewLoc = glGetUniformLocation(shaderProgram, "view");
-        auto projLoc = glGetUniformLocation(shaderProgram, "projection");
+        const auto viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        const auto projLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         const int colorLocation = glGetUniformLocation(shaderProgram, "inputColor");
         glUniform4f(colorLocation, 0, 1, 0, 1.0f);
 
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDrawArrays(GL_TRIANGLES, 0, visibleVertices.size());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
